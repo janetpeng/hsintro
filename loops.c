3864219951c9645a36815603def0d31287abb9db
Charley <noneed@gmail.com>
2012-08-27 22:38:50 +1000

    loops

--- loops.c
+++ loops.c
@@ -6,15 +6,41 @@ void janet() {
   printf("janet\n");
 }
 
-int main() {
-  int i;
-  i = 10;
-  while (i < 100) {
-    // printf("%i\n", i);
-    // i++;
-    i = i + 1;
-  }
-  janet();
-  janet();
-}
+// int main() {
+//   int i;
+//   i = 10;
+//   while (i < 100) {
+//     // printf("%i\n", i);
+//     janet();
+//     // i++;
+//     i = i + 1;
+//   }
+//   janet();
+//   janet();
+// }
 
+int main() {
+  int n = 10;
+  int i,j;
+  for (i = 0; i < n; ++i)
+  {
+    for (j = 0; j < n; ++j)
+    {
+      printf("* ");
+    }
+    printf("\n");
+  }
+
+  printf("\n");
+  i = 0;
+
+  while (i < n) {
+    i++; // ++i
+    j = 0;
+    while (j < n) {
+      j++;
+      printf("* ");
+    }
+    printf("\n");
+  }
+}
